08666795eeb8bf0b204fd5d1d2ffc659755e8211
Lakshmi Balasubramaniam <lrb697@gmail.com>
2022-03-01 10:34:58 -0500

    Initial commit

---
+++ src/HelloWorldProject/HelloWorld.java
@@ -0,0 +1,14 @@
+package HelloWorldProject;
+
+public class HelloWorld {
+
+	/**
+	 * This is the main method.
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Hello, World!");
+	}
+
+}
